a84330767ff4acaed8590f19c1822fe1a2bfe1ba
aaabdulazizobid-cyber <aaabdulazizobid@gmail.com>
2025-08-16 23:35:42 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
-const C='sheep-app-v1';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+const CACHE = 'sheep-cache-v2';
+const CORE = [
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.mode === 'navigate') {
+    e.respondWith(fetch(req).catch(() => caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      if (res.ok && new URL(req.url).origin === location.origin) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(req, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
